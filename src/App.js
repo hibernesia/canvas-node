79a083efa25d215b1b1c409725b0b7b728828349
hibernesia <hibernesia@gmail.com>
2020-10-06 00:03:54 +0700

    add header and toolbox style

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import React, { Component } from 'react'
 import Canvas from './Component/Canvas'
+import Header from './Component/Header'
+import Toolbox from './Component/Toolbox'
 export default class App extends Component {
   render() {
     return (
       <div>
+        <Header/>
+        <Toolbox/>
           <Canvas/>
       </div>
     )
--- src/Component/Toolbox.js
+++ src/Component/Toolbox.js
@@ -1,9 +1,16 @@
 import React from 'react'
+import './css/toolbox.css'
+
 
 export default function Toolbox() {
     return (
         <div>
-            
+            <div class="toolbox">
+                <button>Arrow</button>
+                <button>Node</button>
+                <button>Edge</button>
+                <button>Export</button>
+            </div>
         </div>
     )
 }
